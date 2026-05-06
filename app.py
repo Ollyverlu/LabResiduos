8ec75b427e131f7aa6ed988d4fda569b9792c70c
Ollyverlu <110653756+Ollyverlu@users.noreply.github.com>
2026-05-06 00:23:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Laboratório Virtual de Resíduos")
-st.write("Sistema funcionando 🚀")m
+st.write("Sistema funcionando 🚀")
 st.success("Aplicação carregada com sucesso")
