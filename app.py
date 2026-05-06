fafba1b43024a0a274a4b2b4ed9b662a214683eb
Ollyverlu <110653756+Ollyverlu@users.noreply.github.com>
2026-05-06 01:53:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 import streamlit as st
+import numpy as np
+from datetime import datetime
+st.set_page_config(page_title="Laboratório Virtual IFRJ", layout="wide")
+st.title("🧪 Laboratório Virtual IFRJ")
+st.write("Sistema funcionando 🚀")
