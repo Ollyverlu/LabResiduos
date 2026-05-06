e866b98c5e4ad8648f989a00e2ffda3a59392607
Ollyverlu <110653756+Ollyverlu@users.noreply.github.com>
2026-05-06 00:20:45 -0300

    Fix syntax error in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Laboratório Virtual de Resíduos")
-st.write("Sistema funcionando 🚀")
+st.write("Sistema funcionando 🚀")m
 st.success("Aplicação carregada com sucesso")
